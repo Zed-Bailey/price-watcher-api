9355c18f9424790b0a13bbc13a5e787dadf33f85
Zed <emailzed03@gmail.com>
2022-01-19 10:13:37 +1100

    added routes that require auth
    Create a group of routes that require authentication, these routes pass through the AuthorizedEndpoint middleware function. If the token doesn't exist in the session then an un authorized response will be returned

--- src/main.go
+++ src/main.go
@@ -1,16 +1,48 @@
 package main
 
 import (
+	"RestService/src/controller"
+	"RestService/src/model"
 	"net/http"
 
+	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// https://blog.logrocket.com/how-to-build-a-rest-api-with-golang-using-gin-and-gorm/
+	// https://github.com/Depado/gin-auth-example/blob/master/main.go
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
-	})
+	// initalize db
+	model.SetupDatabase()
+
+	//setup session store
+	r.Use(sessions.Sessions("application_session", sessions.NewCookieStore([]byte("token"))))
+
+	r.POST("/signup", controller.Signup)
+	r.GET("/login", controller.Login)
+
+	private := r.Group("/private")
+	// setup endpoints that can only be accessed with a bearer token
+	private.Use(AuthorizedEndpoint)
+	{
+		private.GET("/items", controller.GetItems)
+		private.GET("/logout", controller.Logout)
+		private.POST("/items", controller.CreateItem)
+	}
+
 	r.Run()
+}
+
+func AuthorizedEndpoint(c *gin.Context) {
+	token := c.Request.Header.Get("Bearer")
+	session := sessions.Default(c)
+	user := session.Get(token)
+	if user == nil {
+		// Abort the request with the appropriate error code
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+		return
+	}
+	// Continue down the chain to handler etc
+	c.Next()
 }
