201db43fde456645c73a6635d134e2fdc007cc41
Zed <emailzed03@gmail.com>
2022-01-24 11:03:20 +1100

    add cors middleware

--- src/main.go
+++ src/main.go
@@ -21,12 +21,12 @@ func main() {
 	r := gin.Default()
 	// initalize db
 	model.SetupDatabase()
-
+	r.Use(CORSMiddleware())
 	//setup session store
 	r.Use(sessions.Sessions("application_session", sessions.NewCookieStore([]byte("token"))))
 
 	r.POST("/signup", controller.Signup)
-	r.GET("/login", controller.Login)
+	r.POST("/login", controller.Login)
 
 	private := r.Group("/private")
 	// setup endpoints that can only be accessed with a bearer token
@@ -42,6 +42,22 @@ func main() {
 	}
 
 	r.Run()
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
 
 func AuthorizedEndpoint(c *gin.Context) {
