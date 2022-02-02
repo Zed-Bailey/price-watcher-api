6f9b660cc1d699549f2285413a4144506a4daeba
Zed <emailzed03@gmail.com>
2022-02-02 15:56:31 +1100

    added some comments and moved the router setup to it's own function

--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"RestService/src/controller"
 	"RestService/src/jobs"
+	"RestService/src/logger"
 	"RestService/src/model"
 	"errors"
 	"net/http"
@@ -10,39 +11,65 @@ import (
 	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"github.com/robfig/cron/v3"
-	"github.com/rs/zerolog/log"
 )
 
 /*
 
-TODO prevent user from logging in multiple times and generating multiple tokens, should return the same token as whats in the session
-
+TODO add/update code documentation
+improvments,
+- calculate percentage change between prices, this will require updating the model
+- batch update on a per user basis and run that in individual go routines rather then per item
+- how to handle link no longer existing?
 */
 
 func main() {
+
+	// setup custom logger
+	logger.SetupLogger()
+	logger.Log.Info().Msg("Initalized Logger")
 
 	// setting up cron job
 	// https://pkg.go.dev/github.com/robfig/cron
 	cj := cron.New()
 	cj.AddFunc("@daily", jobs.CheckSites)
 	defer cj.Stop()
-	log.Info().Msg("Setup cron jobs")
+	logger.Log.Info().Msg("Initalized cron jobs")
 
+	// initalize db
+	model.SetupDatabase()
+	logger.Log.Info().Msg("Initalized Database")
+
+	// initalize router
+	router := SetupRouter()
+	logger.Log.Info().Msg("Initalized Router")
+
+	// start cron jobs and router
+	cj.Start()
+	router.Run()
+}
+
+// Setup the router and api routes
+func SetupRouter() *gin.Engine {
 	// setup gin api routes
 	// https://blog.logrocket.com/how-to-build-a-rest-api-with-golang-using-gin-and-gorm/
 	// https://github.com/Depado/gin-auth-example/blob/master/main.go
 	r := gin.Default()
-	// initalize db
-	model.SetupDatabase()
+
 	r.Use(CORSMiddleware())
+
 	//setup session store
-	r.Use(sessions.Sessions("application_session", sessions.NewCookieStore([]byte("token"))))
+	store := sessions.NewCookieStore([]byte("userID"))
+	store.Options(sessions.Options{MaxAge: 60 * 60 * 12})
+
+	// add session store to the router
+	r.Use(sessions.Sessions("token", store))
 
 	r.POST("/signup", controller.Signup)
 	r.POST("/login", controller.Login)
 
 	private := r.Group("/private")
-	// setup endpoints that can only be accessed with a bearer token
+	// setup endpoints that require an authorized user, uses the AuthorizedEndpoint middleware to
+	// terminate requests from invalid users
 	private.Use(AuthorizedEndpoint)
 	{
 		private.GET("/logout", controller.Logout)
@@ -51,9 +78,8 @@ func main() {
 		private.DELETE("/items/:id", controller.DeleteItem)
 		private.PATCH("/items/:id", controller.UpdateItem)
 	}
-	// start cron jobs and router
-	cj.Start()
-	r.Run()
+
+	return r
 }
 
 func CORSMiddleware() gin.HandlerFunc {
@@ -61,7 +87,7 @@ func CORSMiddleware() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
@@ -80,10 +106,14 @@ func AuthorizedEndpoint(c *gin.Context) {
 		return
 	}
 
-	// token := c.Request.Header.Get("Bearer")
 	session := sessions.Default(c)
-	// get token value
-	user := session.Get(token.Value)
+	// get userID stored in the session
+	user := session.Get("userID")
+	logger.Log.Debug().
+		Str("token", token.Value).
+		Interface("user", user).
+		Msg("Authorized endpoint")
+
 	if user == nil {
 		// Abort the request with the appropriate error code
 		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
