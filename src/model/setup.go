8500db0cd622c86ce584c9e31e0081f7b0b98f4a
Zed <emailzed03@gmail.com>
2022-02-03 15:33:54 +1100

    added a function to initalize an in-memory database instance, useful for testing

--- src/model/setup.go
+++ src/model/setup.go
@@ -8,13 +8,24 @@ import (
 // database instance
 var DB *gorm.DB
 
-// Conect to the database
+// Connect to the database
 func SetupDatabase() {
 	database, err := gorm.Open("sqlite3", "test.db")
 	if err != nil {
 		panic("failed to open database!")
 	}
+	database.AutoMigrate(&User{})
+	database.AutoMigrate(&Product{})
+	DB = database
+}
 
+// Sets up an in memory database that can be used when running test suites
+func SetupDatabaseForTesting() {
+	// https://gorm.io/docs/connecting_to_the_database.html
+	database, err := gorm.Open("sqlite3", "file::memory:?cache=shared")
+	if err != nil {
+		panic("failed to setup in-memory database!")
+	}
 	database.AutoMigrate(&User{})
 	database.AutoMigrate(&Product{})
 	DB = database
